8ec442d6b93e59d8243441561062720492899c21
WIN_NT\x16j051 <x16j051@chiba-fjb.ac.jp>
2017-05-19 11:39:10 +0900

    初回コミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Example11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦";
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(48, 29);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(100, 19);
+            this.tbxYear.TabIndex = 1;
+            this.tbxYear.Text = "（仮）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "年";
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(177, 27);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(75, 23);
+            this.btnJudge.TabIndex = 3;
+            this.btnJudge.Text = "判定";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(130, 75);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 12);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "（空白）";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 111);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxYear);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "うるう年判定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
